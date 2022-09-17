ceb712c0c67b11c72d6129679cbaf4a035b39005
ArturJarosiewicz <artur.jarosiewicz6@gmail.com>
2022-09-17 11:54:37 +0200

    add action1

--- app/src/main/java/com/example/procente/MainActivity.kt
+++ app/src/main/java/com/example/procente/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.procente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.procente.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var number1: Float = 0.0f
+    private var number2: Float = 0.0f
+    private var actionResult: Float = 0.0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        //action1 onClick
+        binding.calculateBtn1.setOnClickListener {
+            action1()
+            resetNumbers()
+        }
+    }
+
+    private fun resetNumbers(){
+        number1= 0.0f
+        number2= 0.0f
+        actionResult= 0.0f
+    }
+
+    private fun action1() {
+        try {
+            number1 = binding.Number1.text.toString().toFloat()
+            number2 = binding.Number2.text.toString().toFloat()
+        } catch (e: Exception) {
+            Toast.makeText(this, "wpisz dane", Toast.LENGTH_SHORT).show()
+        }
+        actionResult = number1 / 100 * number2
+        binding.resultTv1.text = actionResult.toString()
+
     }
 }
